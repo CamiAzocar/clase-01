ab96c3720aa3d5e581f1cb79898c238e481aceff
CamiAzocar <53953312+CamiAzocar@users.noreply.github.com>
2020-03-11 11:33:04 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var a = 50;
+var b = 80
+var c;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  //esto es un comentario
+  /*
+  y esto solo
+  un comentareio mas largo
+  */
+  colorMode(HSB,360,100,100)
+  //background(0,100,100);
+  fill(random(0,360),100,100);
+  noStroke();
+  c = a * random(1,5);
+  ellipse(mouseX,mouseY,c,c)
+}
