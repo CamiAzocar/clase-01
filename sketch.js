d615bd5e5d4ad615fbf679d16396528871ed30bd
CamiAzocar <53953312+CamiAzocar@users.noreply.github.com>
2020-03-11 12:35:21 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,12 +6,7 @@ function setup() {
 }
 
 function draw() {
-  //esto es un comentario
-  /*
-  y esto solo
-  un comentareio mas largo
-  */
-  colorMode(HSB,360,100,100)
+   colorMode(HSB,360,100,100)
   //background(0,100,100);
   fill(random(0,60),100,100);
   noStroke();
