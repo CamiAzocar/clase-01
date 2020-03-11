5188fefddcb70b83feae57572c2fa0c373d6ed5b
CamiAzocar <53953312+CamiAzocar@users.noreply.github.com>
2020-03-11 12:33:38 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var a = 50;
 var b = 80
 var c;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeith);
 }
 
 function draw() {
@@ -13,7 +13,7 @@ function draw() {
   */
   colorMode(HSB,360,100,100)
   //background(0,100,100);
-  fill(random(0,360),100,100);
+  fill(random(0,60),100,100);
   noStroke();
   c = a * random(1,5);
   ellipse(mouseX,mouseY,c,c)
